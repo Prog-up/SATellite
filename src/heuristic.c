a497ce1703971cad1e23656289f8bf036f78c46a
Prog-up <prog_up@orange.fr>
2022-05-24 23:40:24 +0200

    Add 2 versions of jeroslow-wang heuristic +tmp fct

--- src/heuristic.c
+++ src/heuristic.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "../include/types.h"
+#include "../include/parse_dimacs.h"
 
 //---Init
 int n=1;
@@ -24,7 +25,7 @@ int heuristic_first(CNF* formula){ // return the first literal
 //---Heuristic by frequency
 int heuristic_freq(CNF* formula){ //tabular of the most used literals
     int* tab = (int*) malloc ((formula->varc*2) *sizeof (int));
-    for(int i=0 ; i<formula->varc ; i++){
+    for(int i=0 ; i<formula->varc*2 ; i++){
         tab[i] = 0;
     } // all the cases of tab equal to 0
     struct CNF_clause* c = formula->f;
@@ -32,16 +33,16 @@ int heuristic_freq(CNF* formula){ //tabular of the most used literals
         Clause l = c->c;
         while (l->next != NULL){ // parcours of literals
             if(l->l>0){
-                tab[l->l] = tab[l->l]++;
+                tab[l->l]++;
             }else{
-                tab[l->l+formula->varc] = tab[l->l+formula->varc]++;
+                tab[l->l+formula->varc]++;
             }
             l = l->next; 
         }
         c = c->next;
     }
     int max = 0, res = 0;
-    for(int j=0; j<formula->varc; j++){ // search the most used literal 
+    for(int j=0; j<formula->varc*2; j++){ // search the most used literal 
         if(tab[j]>max){
             if(j<formula->varc){
                 res=j;
@@ -63,4 +64,94 @@ int heuristic_random(CNF* formula, int* val){ // return a random literal
             return val[rd];
         }
     }
+}
+
+//---Heuristic by jeroslow-wang with difference betwen a literal and it's negation
+int heuristic_jeroslow_wang_one_sided(CNF* formula){
+    Clause l;
+    Clause l2;
+    int count;
+    int* tab = (int*) malloc ((formula->varc*2) *sizeof (int));
+    for(int i=0 ; i<formula->varc*2 ; i++){
+        tab[i] = 0;
+    } // all the cases of tab equal to 0
+    struct CNF_clause* c = formula->f;
+    while (c->next != NULL){ // parcours of clauses
+        l = c->c;
+
+        l2 = c->c; // count of the literals in this clause
+        count = 0;
+        while (l2->next!=NULL){
+            count++;
+        }
+        
+        while (l->next != NULL){ // parcours of literals
+            if(l->l>0){
+                tab[l->l]+=pow(2, -count);
+            }else{
+                tab[l->l+formula->varc]+=pow(2, -count);
+            }
+            l = l->next; 
+        }
+        c = c->next;
+    }
+    int max = 0, res = 0;
+    for(int j=0; j<formula->varc*2; j++){ // search the literal with better score
+        if(tab[j]>max){
+            if(j<formula->varc){
+                res=j;
+            }else{
+                res=-(j-formula->varc);
+            }
+            max=tab[j];
+        }
+    }
+    free(tab);
+    return res;
+}
+
+//---Heuristic by jeroslow-wang without difference betwen a literal and it's negation
+int heuristic_jeroslow_wang_two_sided(CNF* formula){
+    Clause l;
+    Clause l2;
+    int count;
+    int* tab = (int*) malloc ((formula->varc) *sizeof (int));
+    for(int i=0 ; i<formula->varc ; i++){
+        tab[i] = 0;
+    } // all the cases of tab equal to 0
+    struct CNF_clause* c = formula->f;
+    while (c->next != NULL){ // parcours of clauses
+        l = c->c;
+
+        l2 = c->c; // count of the literals in this clause
+        count = 0;
+        while (l2->next!=NULL){
+            count++;
+        }
+        
+        while (l->next != NULL){ // parcours of literals
+            tab[abs(l->l)]+=pow(2, -count);
+            l = l->next; 
+        }
+        c = c->next;
+    }
+    int max = 0, res = 0;
+    for(int j=0; j<formula->varc; j++){ // search the literal with better score
+        if(tab[j]>max){
+            res=j;
+            max=tab[j];
+        }
+    }
+    free(tab);
+    return res;
+}
+
+//---Tmp, debug
+int main(void){
+    for(int i=0;i<100;i++){
+        print_CNF(parse_cnf("../test/SAT.cnf"));
+        printf("\n");
+        printf("%d", heuristic_first(parse_cnf("../test/SAT.cnf")));
+    }
+    return 0;
 }
